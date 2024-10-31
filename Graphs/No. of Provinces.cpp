82989d21f8d4a554722134fd94c25d610cae4d2d
Adwaith Jayasankar <kichu31084@gmail.com>
2024-10-31 16:10:51 +0530

    Change in No of provinces

--- Graphs/No. of Provinces.cpp
+++ Graphs/No. of Provinces.cpp
@@ -10,20 +10,21 @@ public:
         }
     }
 
-
     int findCircleNum(vector<vector<int>>& adj) {
         int n = adj.size();
+        int m = adj[0].size();
         vector<int> adjLs[n];
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+            for(int j = 0; j < m; j++){
                 if(adj[i][j] == 1 && i != j){
                     adjLs[i].push_back(j);
                     adjLs[j].push_back(i);
                 }
             }
         }
-        vector<int> vis(n, 0);
+
         int count = 0;
+        vector<int> vis(n, 0);
         for(int i = 0; i < n; i++){
             if(!vis[i]){
                 count++;
@@ -34,3 +35,8 @@ public:
         return count;
     }
 };
+
+//Approach
+//(1) First convert the given adjacency matrix into an adjacency list.
+//(2) Next we just need to get a count on how many times the DFS traversal has been called on all the connected components. If the vis array is not marked then only we call the dfs function.
+//(3) Just return the count after that.
