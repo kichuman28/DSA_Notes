6ccd1c014ed665d964299bba3186ba2d9d422390
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-17 17:33:56 +0530

    Binary to decimal

---
+++ Bit Manipulation/Binary To Decimal.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binaryToDecimal(string str){
+    int n = str.size();
+    int ans = 0;
+    int exp = n-1;
+    for(int i = 0; i < n; i++){
+        if(str[i] == '1') ans += pow(2, exp);
+        exp--;
+    }
+    
+    return ans;
+}
+
+
+int main(){
+    //Function to convert binary to decimal
+    cout << binaryToDecimal("111");
+}
