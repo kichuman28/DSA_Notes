af73ea5e70eac73b7eedb66ae6deb9af1f52f611
Adwaith Jayasankar <kichu31084@gmail.com>
2025-01-17 17:38:09 +0530

    alternate way binary to decimal

--- Bit Manipulation/Binary To Decimal.cpp
+++ Bit Manipulation/Binary To Decimal.cpp
@@ -18,3 +18,26 @@ int main(){
     //Function to convert binary to decimal
     cout << binaryToDecimal("111");
 }
+
+
+// Alternate way 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int binaryToDecimal(string str){
+//     int n = str.size();
+//     int ans = 0;
+//     int exp = 1;
+//     for(int i = 0; i < n; i++){
+//         if(str[i] == '1') ans += exp;
+//         exp = exp * 2;
+//     }
+    
+//     return ans;
+// }
+
+
+// int main(){
+//     //Function to convert binary to decimal
+//     cout << binaryToDecimal("111");
+// }
